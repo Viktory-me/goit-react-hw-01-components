bf11b58f2742692cb05fc7282a2c63cf1e82cb41
Viktory-me <73599864+Viktory-me@users.noreply.github.com>
2021-06-26 19:05:01 +0300

    first part done,probably
    propTypes r quest

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import user from './data/user.json';
 export default function App () {
     return <div>
          <Profile
+avatar={user.avatar}
 name={user.name}
 tag={user.tag}
 location={user.location}
--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,32 +1,50 @@
+import PropTypes from 'prop-types'; 
+import defaultAvatar from '../../images/default-avatar.svg';
+import s from './Profile.module.css';
+
 export default function Profile(props) {
-    const { name, tag, location, followersQuantity, viewsQuantity, likesQuantity }= props;
+    const { name, tag, location, followersQuantity, viewsQuantity, likesQuantity, avatar }= props;
     return (
-    <div class="profile" >
-      <div class="description">
+    <div className={s.profile} >
+      <div className={s.description}>
         <img
-          src="https://www.flaticon.com/svg/static/icons/svg/3135/3135715.svg"
+          src={avatar}
           alt="Аватар пользователя"
-          class="avatar"
+          className={s.avatar}
         />
-        <p class="name">{name}</p>
-        <p class="tag">{tag}</p>
-        <p class="location">{location}</p>
+        <p className={s.name}>{name}</p>
+        <p className={s.tag}>@{tag}</p>
+        <p className={s.location}>{location}</p>
       </div>
     
-      <ul class="stats">
+      <ul className={s.stats}>
         <li>
-          <span class="label">Followers</span>
-          <span class="quantity">{followersQuantity}</span>
+          <span className={s.label}>Followers</span>
+          <span className={s.quantity}>{followersQuantity}</span>
         </li>
         <li>
-          <span class="label">Views</span>
-          <span class="quantity">{viewsQuantity}</span>
+          <span className={s.label}>Views</span>
+          <span className={s.quantity}>{viewsQuantity}</span>
         </li>
         <li>
-          <span class="label">Likes</span>
-          <span class="quantity">{likesQuantity}</span>
+          <span className={s.label}>Likes</span>
+          <span className={s.quantity}>{likesQuantity}</span>
         </li>
       </ul>
     </div>
     );
+  };
+  
+  Profile.defaultProps = {
+    avatar: defaultAvatar,
+  };
+
+  Profile.propTypes = {
+name: PropTypes.string.isRequired,
+tag: PropTypes.string.isRequired,
+location: PropTypes.string.isRequired,
+followersQuantity: PropTypes.number.isRequired,
+viewsQuantity: PropTypes.number.isRequired, 
+likesQuantity: PropTypes.number.isRequired, 
+avatar: PropTypes.string,
   };
